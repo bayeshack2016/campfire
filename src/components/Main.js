dd61892238bc970aca246e584fc5a9bcdc54284b
Stephan Gabler <stephan.gabler@gmail.com>
2016-04-23 18:51:13 -0700

    add example plot

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ require('styles/App.css');
 
 import React from 'react'
 import {SiteSuggest} from './site_suggest'
-
+const Plotly = require('react-plotlyjs');
 
 class AppComponent extends React.Component {
 
@@ -12,11 +12,20 @@ class AppComponent extends React.Component {
   }
 
   render() {
+    var data = [
+      {
+        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+        y: [1, 3, 6],
+        type: 'scatter'
+      }
+    ];
+
     return (
       <div>
         <h1>Burn Campfire Burn</h1>
         <SiteSuggest onSuggestSelect={this.handleSuggestSelect}
             placeholder="Your latest job" hitsPerPage={8}/>
+        <Plotly className="whatever" data={data}/>
 
       </div>
     );
